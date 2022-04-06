a84cd315b0def6a304b5ac062b67ba18568ff82b
movesoo <103127692+movesoo@users.noreply.github.com>
2022-04-07 01:46:28 +0900

    sooBranch와 ignore 커밋

---
+++ githublast/movesooBranch.swift
@@ -0,0 +1,20 @@
+//
+//  movesooBranch.swift
+//  githublast
+//
+//  Created by 박성수 on 2022/04/07.
+//
+
+import SwiftUI
+
+struct movesooBranch: View {
+    var body: some View {
+        Text("hello this is movesoo branch")
+    }
+}
+
+struct movesooBranch_Previews: PreviewProvider {
+    static var previews: some View {
+        movesooBranch()
+    }
+}
