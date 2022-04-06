bc1442cd0370130431f7610436c8104ff29f4b08
movesoo <103127692+movesoo@users.noreply.github.com>
2022-04-07 01:31:32 +0900

    Initial commit

---
+++ githublast/githublastApp.swift
@@ -0,0 +1,17 @@
+//
+//  githublastApp.swift
+//  githublast
+//
+//  Created by 박성수 on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct githublastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
