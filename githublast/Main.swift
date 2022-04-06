27d4c5a6defab5998b92abe363617277eb553521
movesoo <103127692+movesoo@users.noreply.github.com>
2022-04-07 01:50:20 +0900

    main에서 커밋

---
+++ githublast/Main.swift
@@ -0,0 +1,20 @@
+//
+//  Main.swift
+//  githublast
+//
+//  Created by 박성수 on 2022/04/07.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Main_Previews: PreviewProvider {
+    static var previews: some View {
+        Main()
+    }
+}
