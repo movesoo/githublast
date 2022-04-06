88646497dba5cb13665f3f1a9a62a815774a3ecc
parksungsoo <72736657+sunshiningsoo@users.noreply.github.com>
2022-04-07 01:36:01 +0900

    Newfile add

---
+++ githublast/NewFile.swift
@@ -0,0 +1,20 @@
+//
+//  NewFile.swift
+//  githublast
+//
+//  Created by 박성수 on 2022/04/07.
+//
+
+import SwiftUI
+
+struct NewFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewFile_Previews: PreviewProvider {
+    static var previews: some View {
+        NewFile()
+    }
+}
